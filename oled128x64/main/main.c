678b4af2a201c1fe3285074d0421157d23051060
Peter <xtiqin@163.com>
2022-07-21 19:51:34 +0800

    update

--- oled128x64/main/main.c
+++ oled128x64/main/main.c
@@ -7,71 +7,37 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-#include "sdkconfig.h"
-
-#include <driver/gpio.h>
-#include <driver/spi_master.h>
-#include <esp_log.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <stdio.h>
-#include <string.h>
 #include <u8g2.h>
 
 #include "sdkconfig.h"
-#include "u8g2_esp32_hal.h"
 
-// SDA - GPIO21
-#define PIN_SDA 21
-
-// SCL - GPIO22
-#define PIN_SCL 22
-
-static const char *TAG = "ssd1306";
-
-void task_test_SSD1306i2c(void *ignore)
-{
-    u8g2_esp32_hal_t u8g2_esp32_hal = U8G2_ESP32_HAL_DEFAULT;
-    u8g2_esp32_hal.sda = PIN_SDA;
-    u8g2_esp32_hal.scl = PIN_SCL;
-    u8g2_esp32_hal_init(u8g2_esp32_hal);
-
-    u8g2_t u8g2; // a structure which will contain all the data for one display
-    u8g2_Setup_ssd1306_i2c_128x32_univision_f(
-        &u8g2, U8G2_R0,
-        // u8x8_byte_sw_i2c,
-        u8g2_esp32_i2c_byte_cb,
-        u8g2_esp32_gpio_and_delay_cb); // init u8g2 structure
-    u8x8_SetI2CAddress(&u8g2.u8x8, 0x78);
-
-    ESP_LOGI(TAG, "u8g2_InitDisplay");
-    u8g2_InitDisplay(&u8g2); // send init sequence to the display, display is in
-                             // sleep mode after this,
-
-    ESP_LOGI(TAG, "u8g2_SetPowerSave");
-    u8g2_SetPowerSave(&u8g2, 0); // wake up display
-    ESP_LOGI(TAG, "u8g2_ClearBuffer");
-    u8g2_ClearBuffer(&u8g2);
-    ESP_LOGI(TAG, "u8g2_DrawBox");
-    u8g2_DrawBox(&u8g2, 0, 26, 80, 6);
-    u8g2_DrawFrame(&u8g2, 0, 26, 100, 6);
-
-    ESP_LOGI(TAG, "u8g2_SetFont");
-    u8g2_SetFont(&u8g2, u8g2_font_ncenB14_tr);
-    ESP_LOGI(TAG, "u8g2_DrawStr");
-    u8g2_DrawStr(&u8g2, 2, 17, "Hi nkolban!");
-    ESP_LOGI(TAG, "u8g2_SendBuffer");
-    u8g2_SendBuffer(&u8g2);
-
-    ESP_LOGI(TAG, "All done!");
-
-    vTaskDelete(NULL);
-}
+#define BLINK_GPIO 12
 
 void app_main(void)
 {
-    task_test_SSD1306i2c();
+    u8g2_t u8g2;
+    u8g2_Setup_ssd1306_128x64_noname_f(&u8g2,U8G2_R0,u8g2_i2c_byte_cb)
+    U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R2, /* clock=*/16, /* data=*/17, /* reset=*/U8X8_PIN_NONE);
+    u8g2.begin();
+    u8g2.firstPage();
+    u8g2.setFont(u8g2_font_ncenB14_tr);
+    u8g2.drawStr(0,24,"Hello World!");
+
+    // gpio_reset_pin(BLINK_GPIO);
+    // /* Set the GPIO as a push/pull output */
+    // gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    // while(1) {
+    //     /* Blink off (output low) */
+    //     printf("Turning off the LED\n");
+    //     gpio_set_level(BLINK_GPIO, 0);
+    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
+    //     /* Blink on (output high) */
+    //     printf("Turning on the LED\n");
+    //     gpio_set_level(BLINK_GPIO, 1);
+    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // }
 }
